342ab4a18ea149267dc3291a73533cae97e14de6
Dub_Andrei <Andry.Dub1986@gmail.com>
2023-03-03 21:46:56 +0300

    changed

--- services/webapp/webapp/asgi.py
+++ services/webapp/webapp/asgi.py
@@ -93,6 +93,8 @@ async def application(scope: Dict, receive: Callable, send: Callable) -> None:
     db_settings = []
     if path == "/~/alexander_sidorov/":
         payload = "Hello from Alexander Sidorov"
+    elif path == "/~/chernousik_ilya/":
+        payload = "Hello from Ilya Chernousik"
     else:
         payload = build_payload(scope, request, db_settings).json(sort_keys=True, indent=2)
 

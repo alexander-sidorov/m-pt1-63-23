6b5fe64bf6d591126e77391207f95a9227a7d049
Mitya24 <dmitriyzhdan23@gmail.com>
2023-03-10 21:03:49 +0300

    changed3

--- services/webapp/webapp/asgi.py
+++ services/webapp/webapp/asgi.py
@@ -95,7 +95,7 @@ async def application(scope: Dict, receive: Callable, send: Callable) -> None:
     if path == "/~/alexander_sidorov":
         payload = "Hello from Alexander Sidorov"
     elif path == "/~/dmitriy_zhdanovich/":
-        payload = "Hello from Dmitriy Zhdanovich"
+        payload = "Hello from Dmitriy Zhdanovich23"
     elif path == "/~/ilya_putrich/":
         payload = "Hello from Ilya Putrich"
     elif path == "/~/maksim_berezovik":

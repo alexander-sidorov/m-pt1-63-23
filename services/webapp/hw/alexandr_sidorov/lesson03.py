475adfc1d52063d956bb63971cc9de41f325caa5
mitya27 <dmitriyzhdan23@gmail.com>
2023-03-14 18:26:10 +0300

    correct mistakes

--- services/webapp/hw/alexandr_sidorov/lesson03.py
+++
@@ -1,4 +0,0 @@
-def view(path: str) -> str | None:
-    if path == "/~/alexander_sidorov":
-        return "Hello from Alexander Sidorov"
-    return None
--- services/webapp/hw/dmitriy_zhdanovich/lesson03.py
+++ services/webapp/hw/dmitriy_zhdanovich/lesson03.py
@@ -1,3 +1,5 @@
-def view(path):
+def view(path: str) -> str | None:
     if path == "/~/dmitriy_zhdanovich":
         return "Hello from Dmitriy Zhdanovich"
+    return None
+
